6bf8584829048d95b5b655ae174870c3c9700235
Sukhankar <126391408+Sukhankar@users.noreply.github.com>
2023-03-15 19:41:08 +0530

    Addition of two numbers in java
    This is the example of addition of two numbers in java

---
+++ Exadd.java
@@ -0,0 +1,21 @@
+
+
+class Exadd
+{
+public static void main(String[] args)
+{
+Add a=new Add();
+a.add();
+}
+}
+
+public class Add
+{
+int a=10,b=20;
+void add()
+{
+int c;
+c=a+b;
+System.out.print("addtion is "+c);
+}
+}
