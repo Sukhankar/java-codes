bcb4a776c2ce759d2ae36a4c53d8b705caf4dac9
Sukhankar <126391408+Sukhankar@users.noreply.github.com>
2023-03-15 19:38:58 +0530

    Example of array in java
    the program contains the program of array in java

---
+++ Exarry.java
@@ -0,0 +1,11 @@
+class Exarry
+{
+public static void main(String[] args)
+{
+int []a={10,20,30,40,5,06,51,96,1023,50,60,70,870,80,90};
+for (int i=0;i<a.length;i++)
+{
+System.out.println(a[i]);
+}
+System.out.println("the length of arry is " +a.length);
+}}
