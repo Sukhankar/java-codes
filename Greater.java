70e35cddc002ee43f7a5bc084b9fb6529c04c970
Sukhankar <126391408+Sukhankar@users.noreply.github.com>
2023-03-20 21:10:41 +0530

    Greatest number example in java
    the code is of java which describes the example of Greatest number

---
+++ Greater.java
@@ -0,0 +1,15 @@
+class Greater
+{
+public static void main(String[] agrs)
+{
+int a=10,b=20; 
+if(b>a)
+{
+System.out.println("b is greater than a");
+}
+else
+{
+System.out.println("a is greater than b");
+}
+}
+}
