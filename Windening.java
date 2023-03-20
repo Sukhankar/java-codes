38c03694fb7c14e13254ff76ccb01077547e2fce
Sukhankar <126391408+Sukhankar@users.noreply.github.com>
2023-03-20 21:19:42 +0530

    Winding casting in java
    this is the example of casting type Winding

---
+++ Windening.java
@@ -0,0 +1,13 @@
+class Windening
+{
+public static void main (String[] args)
+{
+int mynum=100;
+double mydub=mynum;
+
+System.out.println("the integer is " +mynum);
+System.out.println("the double is " +mydub);
+
+}
+}
+
