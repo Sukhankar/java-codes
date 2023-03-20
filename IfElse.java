8cb90b4de6369ed1d8f3d7af21dfecdb941a8328
Sukhankar <126391408+Sukhankar@users.noreply.github.com>
2023-03-20 21:15:55 +0530

    Example conditional statements in java
    the code is of example of if-else statement in java

---
+++ IfElse.java
@@ -0,0 +1,15 @@
+class IfElse
+{
+public static void main(String[] args)
+{
+int a=10,b=20;
+if(a>b)
+{
+System.out.println("a is greater");
+}
+else
+{
+System.out.println("b is greater");
+}
+}
+}
