748e70a330d2afa02c03e59d37d85bf22e7aaf0c
Sukhankar <126391408+Sukhankar@users.noreply.github.com>
2023-03-20 21:06:46 +0530

    Example of instance operator
    this is the example of instance operator in java

---
+++ Exintanceoperator.java
@@ -0,0 +1,10 @@
+class Exinstanceoperator
+{
+public static void main (String[] args)
+{
+Exinstanceoperator s=new Exinstanceoperator();
+System.out.print(s instanceof Exinstanceoperator);
+}
+}
+
+
