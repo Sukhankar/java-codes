24fc81743c02937b465b5b1f400814b0db33e37a
Sukhankar <126391408+Sukhankar@users.noreply.github.com>
2023-03-20 21:17:53 +0530

    Narrowing casting in java
    this is the example of casting of type Narrowing in java

---
+++ Narrowing.java
@@ -0,0 +1,14 @@
+class Narrowing
+{
+public static void main (String[] args)
+{
+
+double mydub=99.1152;
+int mynum=(int) mydub;
+
+System.out.println("the integer is " +mynum);
+System.out.println("the double is " +mydub);
+
+}
+}
+
